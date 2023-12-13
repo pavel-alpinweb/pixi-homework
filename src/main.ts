1c4699c3315d555eaefb6aee04520a479e3507aa
alpinweb <alpinweb@yandex.ru>
2023-12-13 11:25:51 +0600

    PIXI app settings

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log('Project init!');
+import * as PIXI from 'pixi.js';
+import '@pixi/graphics-extras';
+
+const appContainer = <HTMLDivElement>document.getElementById('app');
+console.log(appContainer);
+const app = new PIXI.Application({
+    antialias: true,
+    resizeTo: appContainer,
+    background: '#f5f5f5',
+});
+appContainer.appendChild(<HTMLCanvasElement>app.view);
