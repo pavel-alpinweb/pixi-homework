643ae6762ed2c35f82b8270fa616cea4c0bd61f8
alpinweb <alpinweb@yandex.ru>
2023-12-13 11:39:33 +0600

    create square

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import * as PIXI from 'pixi.js';
 import '@pixi/graphics-extras';
 
+const SQUARE_WIDTH = 250;
+const SQUARE_HEIGHT = 250;
 const appContainer = <HTMLDivElement>document.getElementById('app');
 console.log(appContainer);
 const app = new PIXI.Application({
@@ -9,3 +11,17 @@ const app = new PIXI.Application({
     background: '#f5f5f5',
 });
 appContainer.appendChild(<HTMLCanvasElement>app.view);
+
+const square = new PIXI.Graphics();
+square.beginFill('#fff');
+square.drawRect(
+    window.innerWidth / 2 - SQUARE_WIDTH / 2,
+    window.innerHeight / 2 - SQUARE_HEIGHT / 2,
+    SQUARE_WIDTH,
+    SQUARE_HEIGHT
+);
+square.endFill();
+
+const container = new PIXI.Container();
+container.addChild(square)
+app.stage.addChild(container);
